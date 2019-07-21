92a81f9cc75f2e31cda63ea86621011ee6eece16
Siyu <nimagangfour@gmail.com>
2019-07-21 22:02:19 +1000

    shouldComponentUpdate(nextProps, nextState){ return nextProps.show !== this.props.show}

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,38 @@
-import React from "react";
-import Aux from "../../../hoc/Aux";
+import React, { Component } from "react";
+
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => (
-    <li key={igKey}>
-      <span style={{ textTransform: "capitalize" }}>{igKey}</span> :{" "}
-      {props.ingredients[igKey]}
-    </li>
-  ));
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log("[OrderSummary] componentWillUpdate");
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Your burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.totalPrice.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelledH}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinuedH}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => (
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span> :{" "}
+        {this.props.ingredients[igKey]}
+      </li>
+    ));
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Your burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.totalPrice.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelledH}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinuedH}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
 
 export default OrderSummary;
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import classes from "./Layout.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import Aux from "../Aux/Aux";
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  };
+
+  sideDrawerToggledHandler = () => {
+    // alert("menuClicked");
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToogleClicked={this.sideDrawerToggledHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;

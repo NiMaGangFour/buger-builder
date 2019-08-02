4cada4f0a5c4b11b274a883510e809c0596ce3d6
Siyu <nimagangfour@gmail.com>
2019-08-02 22:14:24 +1000

    add <BrowerRouter>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import BurgerBuilder from "./containers/BugerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,13 +1,22 @@
 import React from "react";
+
 import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary.css";
 
 const CheckoutSummary = props => {
   return (
-    <div>
+    <div className={classes.CheckoutSummary}>
       <h1>Hope this burger is tasty！</h1>
-      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+      <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
+      <Button btnType="Danger" clicked>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked>
+        CONTINUE
+      </Button>
     </div>
   );
 };

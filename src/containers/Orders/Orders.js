ff8f731b0879e9416d1960cf0681127c4d2a66b5
Siyu <nimagangfour@gmail.com>
2019-08-06 22:50:44 +1000

    axios.get(/orders.json)

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
 
 class Orders extends Component {
+  state = {
+    order: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ loading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div>

b32e3f677dcef9214457617b3c518a7f51324aed
Siyu <nimagangfour@gmail.com>
2019-08-06 23:28:36 +1000

    use ... to put elements out of an array or object and create the new one

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Order from "../../components/Order/Order";
 import axios from "../../axios-orders";
-
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 class Orders extends Component {
   state = {
     order: [],
@@ -13,8 +13,15 @@ class Orders extends Component {
     axios
       .get("/orders.json")
       .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
         console.log(res.data);
-        this.setState({ loading: false });
+        this.setState({ loading: false, orders: fetchedOrders });
       })
       .catch(err => {
         console.log(err);
@@ -32,4 +39,5 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
+// export default Orders;
